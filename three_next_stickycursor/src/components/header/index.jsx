099b80d60dbc3b1d664ce619c4fd822d0f3f2d27
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-11-13 19:30:31 +0900

    1113 sticky 마무리

--- three_next_stickycursor/src/components/header/index.jsx
+++ three_next_stickycursor/src/components/header/index.jsx
@@ -1,12 +1,14 @@
 import { forwardRef } from "react";
 import styles from "./style.module.scss";
-
+import Magnetic from "../magnetic";
 const Header = forwardRef(function index(props, ref) {
   return (
     <div className={styles.header}>
-      <div ref={ref} className={styles.burger}>
-        <div className={styles.bounds}></div>
-      </div>
+      <Magnetic>
+        <div className={styles.burger}>
+          <div ref={ref} className={styles.bounds}></div>
+        </div>
+      </Magnetic>
     </div>
   );
 });
