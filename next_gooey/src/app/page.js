158aa1f30b3404d7ee0c9207228a4aca71b2f55d
Lee Sang Yong <ckfflekfxms@gmail.com>
2024-02-05 21:31:28 +0900

    gooey 실습 완료

--- next_gooey/src/app/page.js
+++ next_gooey/src/app/page.js
@@ -1,6 +1,9 @@
-import Image from "next/image";
+"use client";
+import { animate } from "framer-motion";
 import styles from "./page.module.scss";
+import { useEffect, useRef, useState } from "react";
 
+// number shape svg code array
 const numbers = [
   "M87.9,79.2c1.1-0.4,53.7-39.2,54.9-39.1v180.5",
   "M81.7,85.7c-1.4-67,112.3-55.1,90.2,11.6c-12.6,32-70.6,83.7-88.8,113.7h105.8",
@@ -10,10 +13,80 @@ const numbers = [
 ];
 
 export default function Home() {
+  const paths = useRef([]);
+  const circles = useRef([]);
+  const nbOfCircles = 40;
+  const radius = 20;
+  // dynamic하게 숫자들을 바꾸기 위한 상태값 설정
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const length = paths.current[index].getTotalLength();
+    const step = length / nbOfCircles;
+
+    circles.current.forEach((circle, i) => {
+      const { x, y } = paths.current[index].getPointAtLength(i * step);
+      animate(circle, { cx: x, cy: y }, { delay: 0.01 * i, ease: "easeInOut" });
+    });
+  }, [index]);
+
   return (
     <main className={styles.main}>
+      <div className={styles.numbers}>
+        {numbers.map((_, i) => {
+          return (
+            <p
+              style={{ color: i == index ? "red" : "black" }}
+              onClick={() => {
+                setIndex(i);
+              }}>
+              {i + 1}
+            </p>
+          );
+        })}
+      </div>
       <svg viewBox="0 0 256 256">
-        <path d={numbers[1]} />
+        {/* def안은 blur속성에 관련된 것 */}
+        <defs>
+          <filter id="filter">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="20"
+              result="blur"
+            />
+            {/* in은 위 블러의 result. values는 rgba에 대응되는 값들. */}
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 25 -15"
+              result="filter"
+            />
+          </filter>
+        </defs>
+
+        {numbers.map((d, i) => {
+          return (
+            <path
+              key={`p_${i}`}
+              ref={(ref) => (paths.current[i] = ref)}
+              d={d}
+            />
+          );
+        })}
+
+        <g>
+          {[...Array(nbOfCircles)].map((_, i) => {
+            return (
+              <circle
+                key={`c_${i}`}
+                ref={(ref) => (circles.current[i] = ref)}
+                r={radius}
+                cx={128}
+                cy={128}
+              />
+            );
+          })}
+        </g>
       </svg>
     </main>
   );
